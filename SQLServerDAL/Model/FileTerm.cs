31d2aa66ae52f69ad3744e9071d9a0edcbcc2e79
qizl <q@qizl.cn>
2016-04-05 00:41:52 +0800

    实现增删改与sql语句执行、分页等常用操作

---
+++ SQLServerDAL/Model/FileTerm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.EnjoyCodes.Model
+{
+    public class FileTerm
+    {
+        public Guid ID { get; set; }
+        public string Title { get; set; }
+        public string Describe { get; set; }
+        public Guid GenreID { get; set; }
+        public DateTime CreateTime { get; set; }
+        public bool IsNotice { get; set; }
+    }
+}
