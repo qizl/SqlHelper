56be947f3ac16bfa04757c9490bfa47e622884e7
qizl <q@qizl.cn>
2016-04-05 21:45:17 +0800

    -

---
+++ SqlHelper/SqlHelper.Client/FormMain.cs
@@ -0,0 +1,56 @@
+﻿using Com.EnjoyCodes.Model;
+using System;
+using System.Data;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Com.EnjoyCodes.SqlHelper.Client
+{
+    public partial class FormMain : Form
+    {
+        private FileDAL _fileDAL = new FileDAL();
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            // 1.增删改
+            //var fileTerm = new FileTerm()
+            //{
+            //    ID = Guid.NewGuid(),
+            //    Title = "ado.net test",
+            //    Describe = "add from ado.net",
+            //    GenreID = Guid.Empty,
+            //    IsNotice = true,
+            //    CreateTime = DateTime.Now
+            //};
+
+            //this._fileDAL.Add(fileTerm);
+
+            //var fileTerm1 = this._fileDAL.Get(fileTerm.ID);
+            //fileTerm1.Data.CreateTime = DateTime.Now;
+
+            //this._fileDAL.Update(fileTerm1.Data);
+
+            //int r = this._fileDAL.Delete(fileTerm.ID);
+
+            // 2.查询数据集 - DataSet
+            DataSet dataSet = this._fileDAL.Get("SELECT TOP 100 * FROM FILETERMS ORDER BY CREATETIME DESC");
+            StringBuilder sb = new StringBuilder();
+            foreach (DataRow row in dataSet.Tables[0].Rows)
+                sb.Append(row["ID"] + "," + row["Title"] + "," + row["CreateTime"] + Environment.NewLine);
+            Debug.WriteLine(sb.ToString());
+
+            // 3.查询数据集
+            //var fileTerms = this._fileDAL.Get(10);
+            var fileTerms = this._fileDAL.Get(1, 9, string.Empty, "ID");
+
+            // 4.查询表行数
+            int count = this._fileDAL.Count();
+        }
+    }
+}
---
+++ SqlHelper/SqlHelper/FileDAL.cs
@@ -0,0 +1,148 @@
+﻿using Com.EnjoyCodes.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.EnjoyCodes.SqlHelper
+{
+    public class FileDAL
+    {
+        public int Add(FileTerm fileTerm)
+        {
+            string sqlStr = @"INSERT INTO FILETERMS(ID,TITLE,DESCRIBE,GENREID,CREATETIME,ISNOTICE) VALUES(@ID,@TITLE,@DESCRIBE,@GENREID,@CREATETIME,@ISNOTICE)";
+            SqlParameter[] parameters = {
+                new SqlParameter("@ID",SqlDbType.UniqueIdentifier),
+                new SqlParameter("@TITLE",SqlDbType.NVarChar),
+                new SqlParameter("@DESCRIBE",SqlDbType.NVarChar),
+                new SqlParameter("@GENREID",SqlDbType.UniqueIdentifier),
+                new SqlParameter("@CREATETIME",SqlDbType.DateTime),
+                new SqlParameter("@ISNOTICE",SqlDbType.Bit),
+            };
+            parameters[0].Value = fileTerm.ID;
+            parameters[1].Value = fileTerm.Title;
+            parameters[2].Value = fileTerm.Describe;
+            parameters[3].Value = fileTerm.GenreID;
+            parameters[4].Value = fileTerm.CreateTime;
+            parameters[5].Value = fileTerm.IsNotice;
+
+            return SqlHelper.ExecuteNonQuery(SqlHelper.GetConnectionString_RW(this.GetType()), CommandType.Text, sqlStr, parameters);
+        }
+
+        /// <summary>
+        /// 根据ID查询单条数据
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public FileTerm Get(Guid id)
+        {
+            var result = new FileTerm();
+            string sqlStr = @"SELECT TOP 1 * FROM FILETERMS WHERE ID='" + id + "'";
+            return SqlHelper<FileTerm>.Read(SqlHelper.GetConnectionString_RW(this.GetType()), CommandType.Text, sqlStr);
+        }
+
+        /// <summary>
+        /// 查询数据集
+        ///     分页方法
+        /// </summary>
+        /// <param name="startIndex">起始索引，从1开始</param>
+        /// <param name="endIndex"></param>
+        /// <param name="sqlWhere"></param>
+        /// <param name="sqlOrderBy"></param>
+        /// <returns></returns>
+        public List<FileTerm> Get(int startIndex, int endIndex, string sqlWhere, string sqlOrderBy)
+        {
+            List<FileTerm> result = new List<FileTerm>();
+            StringBuilder sqlStr = new StringBuilder();
+            sqlStr.Append("SELECT * FROM (");
+            sqlStr.Append("SELECT ROW_NUMBER() OVER (");
+            if (!string.IsNullOrEmpty(sqlOrderBy.Trim()))
+                sqlStr.Append("ORDER BY T." + sqlOrderBy);
+            else
+                sqlStr.Append("ORDER BY T.ID DESC");
+            sqlStr.Append(")AS ROW, T.* FROM FILETERMS T ");
+            if (!string.IsNullOrEmpty(sqlWhere.Trim()))
+                sqlStr.Append("WHERE " + sqlWhere);
+            sqlStr.Append(") T1 ");
+            sqlStr.AppendFormat("WHERE T1.ROW BETWEEN {0} AND {1}", startIndex, endIndex);
+
+            return SqlHelper<FileTerm>.ReadList(SqlHelper.GetConnectionString_RW(base.GetType()), CommandType.Text, sqlStr.ToString());
+        }
+
+        /// <summary>
+        /// 查询数据集
+        ///     获取前n行数据
+        /// </summary>
+        /// <param name="topCount"></param>
+        /// <param name="sqlWhere"></param>
+        /// <param name="sqlOrderBy"></param>
+        /// <returns></returns>
+        public List<FileTerm> Get(int topCount, string sqlWhere, string sqlOrderBy)
+        { return this.Get(1, topCount, sqlWhere, sqlOrderBy); }
+
+        /// <summary>
+        /// 查询数据集
+        ///     获取前n行数据
+        /// </summary>
+        /// <param name="topCount"></param>
+        /// <returns></returns>
+        public List<FileTerm> Get(int topCount)
+        { return this.Get(topCount, string.Empty, "ID"); }
+
+        /// <summary>
+        /// 自定义sql查询数据集
+        /// </summary>
+        /// <param name="sqlStr"></param>
+        /// <returns></returns>
+        public DataSet Get(string sqlStr)
+        { return SqlHelper.ExecuteDataSet(SqlHelper.GetConnectionString_RW(this.GetType()), CommandType.Text, sqlStr); }
+
+        public int Update(FileTerm fileTerm)
+        {
+            string sqlStr = "UPDATE FILETERMS SET TITLE=@TITLE,DESCRIBE=@DESCRIBE,GENREID=@GENREID,CREATETIME=@CREATETIME,ISNOTICE=@ISNOTICE WHERE ID=@ID";
+            SqlParameter[] parameters = {
+                new SqlParameter("@ID",SqlDbType.UniqueIdentifier),
+                new SqlParameter("@TITLE",SqlDbType.NVarChar),
+                new SqlParameter("@DESCRIBE",SqlDbType.NVarChar),
+                new SqlParameter("@GENREID",SqlDbType.UniqueIdentifier),
+                new SqlParameter("@CREATETIME",SqlDbType.DateTime),
+                new SqlParameter("@ISNOTICE",SqlDbType.Bit),
+            };
+            parameters[0].Value = fileTerm.ID;
+            parameters[1].Value = fileTerm.Title;
+            parameters[2].Value = fileTerm.Describe;
+            parameters[3].Value = fileTerm.GenreID;
+            parameters[4].Value = fileTerm.CreateTime;
+            parameters[5].Value = fileTerm.IsNotice;
+
+            return SqlHelper.ExecuteNonQuery(SqlHelper.GetConnectionString_RW(this.GetType()), CommandType.Text, sqlStr, parameters);
+        }
+
+        public int Delete(Guid id)
+        {
+            string sqlStr = @"DELETE FROM FILETERMS WHERE ID='" + id + "'";
+            return SqlHelper.ExecuteNonQuery(SqlHelper.GetConnectionString_RW(this.GetType()), CommandType.Text, sqlStr);
+        }
+
+        /// <summary>
+        /// 查询表行数
+        /// </summary>
+        /// <param name="sqlWhere"></param>
+        /// <returns></returns>
+        public int Count(string sqlWhere = "")
+        {
+            StringBuilder sqlStr = new StringBuilder();
+            sqlStr.Append("SELECT COUNT(1) FROM FILETERMS ");
+            if (!string.IsNullOrEmpty(sqlWhere.Trim()))
+                sqlStr.Append("WHERE " + sqlWhere);
+
+            object obj = SqlHelper.ExecuteScalar(SqlHelper.GetConnectionString_RW(this.GetType()), CommandType.Text, sqlStr.ToString());
+
+            return obj == null ? 0 : Convert.ToInt32(obj);
+        }
+    }
+}
