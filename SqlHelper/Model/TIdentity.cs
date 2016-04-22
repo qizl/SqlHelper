659ae32a2ddf24089255475efd3fb7d2206426c7
qizl <qizl@guanhuai365.com>
2016-04-22 10:07:40 +0800

    添加枚举类型

--- SqlHelper/Model/TIdentity.cs
+++ SqlHelper/Model/TIdentity.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace Com.EnjoyCodes.Model
 {
+    public enum Types
+    {
+        T1 = 1,
+        T2,
+        T3
+    }
+
     public class TIdentity
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public Types Type { get; set; }
         public DateTime CreateTime { get; set; }
     }
 }
