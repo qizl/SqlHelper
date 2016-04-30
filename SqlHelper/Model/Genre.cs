f04212b0d8c70c6ea66d0c3a77aaac9c62cdfa40
qizl <qizl@guanhuai365.com>
2016-04-30 12:35:40 +0800

    -

---
+++ SqlHelper/Model/Genre.cs
@@ -0,0 +1,17 @@
+﻿using Com.EnjoyCodes.SqlAttribute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.EnjoyCodes.Model
+{
+    [Table(Name = "Genres", Prefix = "g_")]
+    public class Genre
+    {
+        [Key]
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+    }
+}
